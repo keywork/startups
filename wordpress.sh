3e3581a0f55c40cceb890dafe74ec8dfd1a74fc9
keywork <smahoney@constant.com>
2014-09-10 12:26:58 -0400

    Update wordpress.sh
    Added lynx installation and root user .my.cnf file for auto login.

--- wordpress.sh
+++ wordpress.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 #Creating Random WP Database Credenitals
 install_dir="/var/www/html"
-blog_title="Default Wordpress Install"
+blog_title="Default Vultr Wordpress Install"
 admin_mail="someone@example.com"
 admin_password="123456"
 
@@ -14,6 +14,7 @@ mysqlrootpass=`date |md5sum |cut -c '1-12'`
 ####  Install Packages for https and mysql
 yum -y install httpd httpd-devel 
 yum -y install mysql mysql-server mysql-devel
+yum -y install lynx
 ##### Open firewall for http and SSL
 iptables -F
 iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
@@ -31,7 +32,11 @@ chkconfig mysqld on
 /usr/bin/mysql -e "USE mysql;"
 /usr/bin/mysql -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
 /usr/bin/mysql -e "FLUSH PRIVILEGES;"
-
+touch /root/.my.cnf
+chmod 640 /root/.my.cnf
+echo "[client]">/root/.my.cnf
+echo "user=root">/root/.my.cnf
+echo "password="$mysqlrootpass>/root/.my.cnf
 ##Install PHP
 yum -y install php php-common php-mysql php-gd php-mbstring php-mcrypt php-xml php-devel
 
@@ -63,8 +68,8 @@ grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
 /bin/sed -i '/**#@/,/$p/d' $install_dir/wp-config.php
 /usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> $install_dir/wp-config.php
 /bin/cat /tmp/wp-tmp-config >> $install_dir/wp-config.php && rm /tmp/wp-tmp-config -f
-/usr/bin/mysql -u root -p $mysqlrootpass -e "CREATE DATABASE $db_name"
-/usr/bin/mysql -u root -p $mysqlrootpass -e "GRANT ALL PRIVILEGES ON $db_name.* to '"$db_user"'@'localhost' IDENTIFIED BY '"$db_password"';"
+/usr/bin/mysql -u root $mysqlrootpass -e "CREATE DATABASE $db_name"
+/usr/bin/mysql -u root $mysqlrootpass -e "GRANT ALL PRIVILEGES ON $db_name.* to '"$db_user"'@'localhost' IDENTIFIED BY '"$db_password"';"
 
 /usr/bin/php -r "
 include '"$install_dir"/wp-admin/install.php';
@@ -75,3 +80,4 @@ echo "Database Name: " $db_name
 echo "Database User: " $db_user
 echo "Database Password: " $db_password
 echo "Mysql root password: " $mysqlrootpass
+
